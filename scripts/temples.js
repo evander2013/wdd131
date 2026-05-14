be4e36af160cf78df17d0c3cb049590d26e0d364
evander2013 <kalwanyie@gmail.com>
2026-05-14 22:14:14 +0300

    changes in all files

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,25 +1,13 @@
-// Footer Dates
-
 const year = document.querySelector("#currentyear");
 const lastModified = document.querySelector("#lastModified");
 
 year.textContent = new Date().getFullYear();
+lastModified.textContent = `Last Modified: ${document.lastModified}`;
 
-lastModified.textContent =
-`Last Modification: ${document.lastModified}`;
-
-
-// Hamburger Menu
-
-const menuButton = document.querySelector("#menu-button");
-const navigation = document.querySelector("nav");
+const menuButton = document.querySelector("#menu");
+const nav = document.querySelector(".navigation");
 
 menuButton.addEventListener("click", () => {
-    navigation.classList.toggle("open");
-
-    if (navigation.classList.contains("open")) {
-        menuButton.textContent = "✖";
-    } else {
-        menuButton.textContent = "☰";
-    }
+    nav.classList.toggle("open");
+    menuButton.classList.toggle("open");
 });
